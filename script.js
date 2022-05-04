f9fcfebe5b2dea6d0140f529d35fa5e63fb64df8
CodeVSC <49163043+CodeVSC@users.noreply.github.com>
2022-05-04 11:24:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function sett() {
+    var img = document.createElement("img");
+    img.src = "https://raw.githubusercontent.com/CodeVSC/assets.github.files/main/leftplayer.png";
+    document.body.appendChild(img);
+}
